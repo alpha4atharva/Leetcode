89832c6cbd6481e3623a734d1e8ea7d6ace7b7ff
Atharva Rana <ranaatharva@gmail.com>
2025-01-31 16:56:09 +0530

    Time: 353 ms (25.93%) | Memory: 175 MB (24.28%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,6 +1,7 @@
 class Solution {
     int ans=0,n,m,color;
 public:
+    // dfs
     void dfs(vector<vector<int>> &grid,int &curr,int i,int j,int color){
         curr++;
         grid[i][j]=color;
@@ -9,17 +10,19 @@ public:
         if(j-1>=0 && grid[i][j-1]==1)   dfs(grid,curr,i,j-1,color);
         if(j+1<m && grid[i][j+1]==1)    dfs(grid,curr,i,j+1,color);
     }
+
     int largestIsland(vector<vector<int>>& grid) {
         n=grid.size(),m=grid[0].size();
-        color=2;
-        unordered_map<int,int> mp;
+        // coloring of different islands
+        color=2; // 0 and 1 initial values in grid
+        unordered_map<int,int> mp; // color->size of island
         mp[0]=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     int curr=0;
                     dfs(grid,curr,i,j,color);
-                    mp[color]=curr;
+                    mp[color]=curr; // color->size of current island
                     color++;
                     ans=max(ans,curr);
                 }
@@ -28,7 +31,7 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==0){
+                if(grid[i][j]==0){ // checking if different islands can be connected through it
                     int curr=1;
                     set<int> st;
                     if(i-1>=0) st.insert(grid[i-1][j]); 
