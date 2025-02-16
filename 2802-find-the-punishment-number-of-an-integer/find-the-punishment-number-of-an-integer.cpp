ebcd79d99308c1eb96369a561832c87b010b70b7
Atharva Rana <ranaatharva@gmail.com>
2025-02-16 18:06:19 +0530

    Time: 95 ms (66.01%) | Memory: 10.1 MB (65.02%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
-    int punishmentNumber(int n) {
-        vector<int> arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,
-                           657,675,703,756,792,909,918,945,964,990,991,999,1000};
-        int sum = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            if(arr[i] <= n) sum += arr[i] * arr[i];
-            else break;
+    bool rec(string num,int idx,int target){
+        if(idx==num.size()) return target==0;
+        
+        int sum=0;
+        for(int i=idx;i<num.size();i++){
+            sum=sum*10+(num[i]-'0');
+            if(sum>target)  break;
+            if(rec(num,i+1,target-sum)) return true;
         }
-        return sum;
+
+        return false;
+    }
+
+    int punishmentNumber(int n) {
+       int sum=0;
+       string temp;
+       for(int i=1;i<=n;i++){
+        temp=to_string(i*i);
+        if(rec(temp,0,i)){
+            sum+=(i*i);
+        }
+       }
+
+       return sum;
     }
 };
