d493dfbb1e5ad27e30bf6cc46497b667773bc464
Atharva Rana <ranaatharva@gmail.com>
2025-02-18 21:44:26 +0530

    Time: 103 ms (8.00%) | Memory: 35 MB (5.68%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -10,15 +10,15 @@ public:
         for(int i=0;i<n;i++){
             ans=pq.top();
             pq.pop();
-            if(st.count(2*ans)==0){
+            if(st.find(2*ans)==st.end()){
                 st.insert(2*ans);
                 pq.push(2*ans);
             }
-            if(st.count(3*ans)==0){
+            if(st.find(3*ans)==st.end()){
                 st.insert(3*ans);
                 pq.push(3*ans);
             }
-            if(st.count(5*ans)==0){
+            if(st.find(5*ans)==st.end()){
                 st.insert(5*ans);
                 pq.push(5*ans);
             }
