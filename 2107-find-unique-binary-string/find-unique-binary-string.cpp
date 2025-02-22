0d917bcb4c06b65211df36fe3270a7cf8f986a34
Atharva Rana <ranaatharva@gmail.com>
2025-02-22 21:50:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (40.19%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,28 +1,37 @@
 class Solution {
-    bool flag=false;
-    unordered_map<string,int> mp;
 public:
-    void rec(string &ans,int idx,int &n){
-        if(idx==n){
-            if(mp.find(ans)==mp.end()){
-                flag=true;
-            }
-            return;
+    bool flag=false;
+    void rec(string &ans,int idx,int n,unordered_map<string,int> &mp){
+    // Base case
+    if(idx==n){
+        // Check
+        if(mp.find(ans)==mp.end()){
+            flag=true;
         }
+        return;
+    }
 
-        ans[idx]='0';
-        rec(ans,idx+1,n);
-        if(flag)    return;;
-        ans[idx]='1';
-        rec(ans,idx+1,n);
+    // Put 0 at idx
+    ans[idx]='0';
+    rec(ans,idx+1,n,mp);
+
+    if(flag==true)  return;
+
+    // Put 1 at idx
+    ans[idx]='1';
+    rec(ans,idx+1,n,mp);
+
     }
 
     string findDifferentBinaryString(vector<string>& nums) {
+        int n=nums.size();
+        unordered_map<string,int> mp;
         for(auto &it:nums)  mp[it]++;
 
-        int n=nums[0].length();
         string ans(n,' ');
-        rec(ans,0,n);
+
+        rec(ans,0,n,mp);
+
         return ans;
     }
 };
