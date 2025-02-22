477afeee98365bf919cbb13f8ecb3f8c054d8e13
Atharva Rana <ranaatharva@gmail.com>
2025-02-22 22:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (90.09%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    void rec(string &ans,int idx,int &n,int &k){
-        if(idx==n){
-            k--;
-            return;
-        }
+    int count=0;
+    void rec(string &ans,int idx,int n,int k){
+    // Base case
+    if(idx==n){
+        count++;
+        return;
+    }
 
-        for(char c='a';c<='c';c++){
-            if(idx>0 && ans[idx-1]==c)  continue;
-            if(k==0)    return;
-            ans[idx]=c;        
-            rec(ans,idx+1,n,k);
-        }
+    // Put at idx
+    for(char z='a';z<='c';z++){
+        if(idx>0 && ans[idx-1]==z)   continue;
+        ans[idx]=z;
+        rec(ans,idx+1,n,k);
+        if(count==k)    break;
+    }
     }
 
     string getHappyString(int n, int k) {
         string ans(n,' ');
         rec(ans,0,n,k);
-        if(k>0) return "";
+        if(k>count) return "";
         return ans;
     }
 };
