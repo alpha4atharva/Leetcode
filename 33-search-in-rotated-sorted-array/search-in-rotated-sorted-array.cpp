21ad0dff1a2e4039df2bffb016b91a557d69e1d4
Atharva Rana <ranaatharva@gmail.com>
2025-02-24 12:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (3.54%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,23 +6,14 @@ public:
             int mid=low+(high-low)/2;
             if(nums[mid]==target)   return mid;
             if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && nums[mid]>target){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
+                if(nums[low]<=target && nums[mid]>target)   high=mid-1;
+                else low=mid+1;
             }
             else{
-                if(nums[high]>=target && nums[mid]<target){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+                if(nums[high]>=target && nums[mid]<target)  low=mid+1;
+                else high=mid-1;
             }
         }
-
         return -1;
     }
 };
