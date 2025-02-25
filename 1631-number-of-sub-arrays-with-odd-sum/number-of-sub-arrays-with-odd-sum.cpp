f7d611fff31e1790a8b760d791f49224ace0b07a
Atharva Rana <ranaatharva@gmail.com>
2025-02-25 18:35:59 +0530

    Time: 15 ms (20.27%) | Memory: 122.5 MB (14.77%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -3,9 +3,10 @@ public:
     int MOD=1e9+7;
     int numOfSubarrays(vector<int>& arr) {
         int n=arr.size();
+        int ans=0;
         vector<int> dp1(n,0),dp2(n,0);
         if(arr[0]%2==0) dp1[0]=1;
-        else dp2[0]=1;
+        else dp2[0]=1,ans++;
         for(int i=1;i<n;i++){
             if(arr[i]%2==0){
                 dp1[i]=dp1[i-1]+1;
@@ -15,10 +16,9 @@ public:
                 dp2[i]=dp1[i-1]+1;
                 dp1[i]=dp2[i-1];
             }
+            ans=(ans+dp2[i])%MOD;
         }
 
-        int ans=0;
-        for(auto it:dp2) ans=(ans+it)%MOD;
         return ans;
     }
 };
