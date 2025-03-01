8a8ee0fcdcc2fe6935df0e9e3dcf4b8e3bbceeec
Atharva Rana <ranaatharva@gmail.com>
2025-03-01 12:19:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
-    bool isPalindrome(string &s,int i,int j){
-        while(i<j){
-            if(s[i]!=s[j]) return false;
-            i++,j--;
+    int palindromeCount(string &s,int i,int j){
+        int count=0;
+        while(i>=0 && j<s.length() && s[i]==s[j]){
+            i--,j++,count++;
         }
-        return true;
+        return count;
     }
+
     int countSubstrings(string s) {
         int ans=0,n=s.length();
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)) ans++;
-            }
+            int even=palindromeCount(s,i,i+1);
+            int odd=palindromeCount(s,i,i);
+            ans+=even+odd;
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(string &s,int i,int j){
+//         while(i<j){
+//             if(s[i]!=s[j]) return false;
+//             i++,j--;
+//         }
+//         return true;
+//     }
+//     int countSubstrings(string s) {
+//         int ans=0,n=s.length();
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(isPalindrome(s,i,j)) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
