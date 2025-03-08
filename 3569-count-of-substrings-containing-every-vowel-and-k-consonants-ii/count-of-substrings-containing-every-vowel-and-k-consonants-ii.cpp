3b273482a824cdec8cbe10cde62ff31459bee053
Atharva Rana <ranaatharva@gmail.com>
2025-03-08 12:51:23 +0530

    Time: 538 ms (16.18%) | Memory: 58.5 MB (14.41%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -6,43 +6,81 @@ public:
         return false;
     }
 
-    long long atmost(string word,int k){
-        if(k<0) return 0;
-        unordered_map<char,int> mp;
-        int vowel=0,tvowel=0,i=0,j=0;
-        long long ans=0;
-        int n=word.length();
-        while(j<n){
-            if(isVowel(word[j])){
-                tvowel++;
-                if(mp.find(word[j])!=mp.end()){
-                    if(mp[word[j]]<i) vowel++;
-                    mp[word[j]]=j;
-                }
-                else{
-                    mp[word[j]]=j;
-                    vowel++;
-                }
-            }
+    // long long atmost(string word,int k){
+    //     if(k<0) return 0;
+    //     unordered_map<char,int> mp;
+    //     int vowel=0,tvowel=0,i=0,j=0;
+    //     long long ans=0;
+    //     int n=word.length();
+    //     while(j<n){
+    //         if(isVowel(word[j])){
+    //             tvowel++;
+    //             if(mp.find(word[j])!=mp.end()){
+    //                 if(mp[word[j]]<i) vowel++;
+    //                 mp[word[j]]=j;
+    //             }
+    //             else{
+    //                 mp[word[j]]=j;
+    //                 vowel++;
+    //             }
+    //         }
 
-            while((j-i+1-tvowel)>k){
-                if(isVowel(word[i])){
-                    if(mp[word[i]]==i) vowel--;
-                    tvowel--;
-                }
-                i++;
-            }
+    //         while((j-i+1-tvowel)>k){
+    //             if(isVowel(word[i])){
+    //                 if(mp[word[i]]==i) vowel--;
+    //                 tvowel--;
+    //             }
+    //             i++;
+    //         }
 
-            if(vowel==5){
-                int x=min({mp['a'],mp['e'],mp['i'],mp['o'],mp['u']});
-                ans+=(x-i+1);
-            }
-            j++;
-        }
-        return ans;
-    }
+    //         if(vowel==5){
+    //             int x=min({mp['a'],mp['e'],mp['i'],mp['o'],mp['u']});
+    //             ans+=(x-i+1);
+    //         }
+    //         j++;
+    //     }
+    //     return ans;
+    // }
 
     long long countOfSubstrings(string word, int k) {
-        return atmost(word,k)-atmost(word,k-1);
+        int n=word.length();
+        unordered_map<char,int> vowels;
+        int c=0;
+        long long ans=0;
+        
+        vector<int> nextConsonant(n);
+        int lastConsonant=n;
+        for(int i=n-1;i>=0;i--){
+            nextConsonant[i]=lastConsonant;
+            if(!isVowel(word[i])) lastConsonant=i;
+        }
+
+        int l=0,r=0;
+        while(r<n){
+            if(isVowel(word[r])) vowels[word[r]]++;
+            else c++;
+
+            while(l<=r && c>k){
+                if(isVowel(word[l])){
+                    vowels[word[l]]--;
+                    if(vowels[word[l]]==0) vowels.erase(word[l]);
+                }
+                else c--;
+                l++;
+            }
+
+            while(l<r && vowels.size()==5 && c==k){
+                ans+=(nextConsonant[r]-r);
+                if(isVowel(word[l])){
+                    vowels[word[l]]--;
+                    if(vowels[word[l]]==0) vowels.erase(word[l]);
+                }
+                else c--;
+                l++;
+            }
+            r++;
+        }
+
+        return ans;
     }
 };
