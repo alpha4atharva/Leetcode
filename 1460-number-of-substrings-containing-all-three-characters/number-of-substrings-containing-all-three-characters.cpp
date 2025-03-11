857d72000bc65455eb9908a8708adc3c71dd222b
Atharva Rana <ranaatharva@gmail.com>
2025-03-11 09:36:00 +0530

    Time: 18 ms (34.12%) | Memory: 11.3 MB (17.77%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -4,15 +4,20 @@ public:
         int n=s.length();
         int ans=0,i=0,j=0;
         unordered_map<char,int> mp;
-        while(i<n){
-            mp[s[i]]++;
-            while(mp.size()==3){
-                mp[s[j]]--;
-                if(mp[s[j]]==0) mp.erase(s[j]);
-                j++;
-            }
-            ans+=j;
-            i++;
+        // while(i<n){
+        //     mp[s[i]]++;
+        //     while(mp.size()==3){
+        //         mp[s[j]]--;
+        //         if(mp[s[j]]==0) mp.erase(s[j]);
+        //         j++;
+        //     }
+        //     ans+=j;
+        //     i++;
+        // }
+
+        for(i=0;i<n;i++){
+            mp[s[i]]=i+1;
+            ans+=min({mp['a'],mp['b'],mp['c']});
         }
         return ans;
     }
