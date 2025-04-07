201a871a00dc6bdbf5c15361ee6ba6ff72491284
Atharva Rana <ranaatharva@gmail.com>
2025-04-07 19:09:26 +0530

    Time: 64 ms (86.06%) | Memory: 100.5 MB (30.34%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool recur(int idx,int target,vector<int>&nums,vector<vector<int>> &memo){
+        if(target==0)   return true;
+        if(idx==nums.size() || target<0) return false;
+        if(memo[idx][target]!=-1)   return memo[idx][target];
+        return memo[idx][target]=recur(idx+1,target-nums[idx],nums,memo)||recur(idx+1,target,nums,memo);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size(); 
+        int totalsum=accumulate(nums.begin(),nums.end(),0);
+        if(totalsum%2==1)   return false;
+        int targetsum=totalsum/2;
+        vector<vector<int>> memo(n,vector<int>(targetsum+1,-1));
+        return recur(0,targetsum,nums,memo);
+    }
+};
