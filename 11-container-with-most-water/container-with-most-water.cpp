80cd22dc15a731171fa5d5de3b570c6ea5f7a4f4
Atharva Rana <ranaatharva@gmail.com>
2025-05-10 23:19:45 +0530

    Time: 0 ms (100.00%) | Memory: 62.9 MB (48.28%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-   Solution(){
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-   }
-public:
     int maxArea(vector<int>& height) {
         int i=0;
         int j=height.size()-1;
