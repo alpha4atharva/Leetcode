192d01462db30faf1af2b4028922d4762ebb31a0
Atharva Rana <ranaatharva@gmail.com>
2025-06-07 12:43:40 +0530

    Time: 152 ms (41.88%) | Memory: 25.9 MB (78.90%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,21 +1,39 @@
+struct Compare{
+    bool operator()(const pair<char,int>&a, const pair<char,int>&b){
+        if(a.first==b.first)
+            return a.second<b.second; // Max-heap on second
+        return a.first>b.first;       // Min-heap on first
+    }
+};
+
 class Solution {
 public:
     string clearStars(string s) {
-        vector<stack<int>> freq(26);
-        for(int i=0;i<s.size();i++){
+        priority_queue<pair<char,int>,vector<pair<char,int>>,Compare> pq;
+        for(int i=0;i<s.length();i++){
             if(s[i]=='*'){
-                for(int i=0;i<26;i++){
-                    if(!freq[i].empty()){
-                        s[freq[i].top()]='*';
-                        freq[i].pop();
-                        break;
-                    }
-                }
+                s[pq.top().second]='*';
+                pq.pop();
             }
             else{
-                freq[s[i]-'a'].push(i);
+                pq.push({s[i],i});
             }
         }
+        // vector<stack<int>> freq(26);
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='*'){
+        //         for(int i=0;i<26;i++){
+        //             if(!freq[i].empty()){
+        //                 s[freq[i].top()]='*';
+        //                 freq[i].pop();
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     else{
+        //         freq[s[i]-'a'].push(i);
+        //     }
+        // }
 
         string ans="";
         for(auto &c:s){
