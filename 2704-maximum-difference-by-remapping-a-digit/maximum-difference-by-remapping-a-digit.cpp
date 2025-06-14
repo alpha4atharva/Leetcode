9b674ccf97865e90e061e6bd2fdc712229d96f52
Atharva Rana <ranaatharva@gmail.com>
2025-06-14 10:41:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (37.74%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s=to_string(num);
+        char x='.';
+        for(auto c:s){
+            if(c!='9'){
+                x=c;
+                break;
+            }
+        }
+        
+        if(x=='.') return num;
+        string s1="",s2="";
+        for(auto &c:s){
+            if(c==x){
+                s1+='9';
+            }
+            else{
+                s1+=c;
+            }
+            if(c==s[0]){
+                s2+='0';
+            }
+            else{
+                s2+=c;
+            }
+        }
+
+        int a=stoi(s1),b=stoi(s2);
+        cout<<a<<" "<<b;
+        return a-b;
+    }
+};
