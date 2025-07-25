62d40acf2d538c8c1b0e8622590983b6a4fafcb9
Atharva Rana <ranaatharva@gmail.com>
2025-07-25 10:44:00 +0530

    Time: 405 ms (20.15%) | Memory: 135.5 MB (23.95%) - LeetSync

--- 2256-count-words-obtained-after-adding-a-letter/count-words-obtained-after-adding-a-letter.cpp
+++ 2256-count-words-obtained-after-adding-a-letter/count-words-obtained-after-adding-a-letter.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int wordCount(vector<string>& start, vector<string>& target) {
-        set<vector<int>> s;
+        set<string> s;
         for(int i=0;i<start.size();i++){
-            vector<int> temp(26,0);
+            string str="00000000000000000000000000";
             for(auto c:start[i]){
-                temp[c-'a']++;
+                str[c-'a']++;
             }
-            s.insert(temp);
+            s.insert(str);
         }
         
         int ans=0;
         for(int i=0;i<target.size();i++){
-            vector<int> temp(26,0);
+            string str="00000000000000000000000000";
             for(auto c:target[i]){
-                temp[c-'a']++;
+                str[c-'a']++;
             }
 
             for(int j=0;j<26;j++){
-                if(temp[j]==1){
-                    temp[j]=0;
-                    if(s.find(temp)!=s.end()){
+                if(str[j]=='1'){
+                    str[j]='0';
+                    if(s.find(str)!=s.end()){
                         ans++;
                         break;
                     }
-                    temp[j]=1;
+                    str[j]='1';
                 }
             }
         }
