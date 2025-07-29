74e653bd0b7f8a118bf6e3756f2d8303175a2bbd
Atharva Rana <ranaatharva@gmail.com>
2025-07-29 15:08:38 +0530

    Time: 2 ms (62.20%) | Memory: 9.8 MB (17.89%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-    bool rec(int i, int j, string &s, string &p) {
+    bool rec(int i, int j, string &s, string &p, vector<vector<int>> &memo) {
         //Base case:pattern end
         if(j==p.size()) return i==s.size();
+
+        if(memo[i][j]!=-1) return memo[i][j];
         
         //Check if current characters match
         bool firstMatch=(i<s.size()) && (s[i]==p[j] || p[j]=='.');
@@ -12,15 +14,17 @@ public:
             //Two choices:
             //1. Skip the "x*" pattern (zero matches)
             //2. Use the "x*" pattern if current chars match
-            return rec(i,j+2,s,p) || (firstMatch && rec(i+1,j,s,p));
+            return memo[i][j]=rec(i,j+2,s,p,memo) || (firstMatch && rec(i+1,j,s,p,memo));
         } 
         else{
             //Regular character or '.' - must match exactly
-            return firstMatch && rec(i+1,j+1,s,p);
+            return memo[i][j]=firstMatch && rec(i+1,j+1,s,p,memo);
         }
     }
     
     bool isMatch(string s, string p) {
-        return rec(0,0,s,p);
+        int n=s.size(),m=p.size();
+        vector<vector<int>> memo(n+1,vector<int>(m+1,-1));
+        return rec(0,0,s,p,memo);
     }
 };
