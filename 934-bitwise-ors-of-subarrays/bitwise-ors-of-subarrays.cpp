59d0338725cb00a6c5f6190121f631483fef4c19
Atharva Rana <ranaatharva@gmail.com>
2025-07-31 17:23:12 +0530

    Time: 862 ms (81.17%) | Memory: 324.3 MB (66.27%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int subarrayBitwiseORs(vector<int>& arr) {
         unordered_set<int> all;    //Stores all unique OR results.
-        unordered_set<int> prev;             //Stores ORs for subarrays ending at previous index.
+        set<int> prev;             //Stores ORs for subarrays ending at previous index.
 
         for(int x:arr){
-            unordered_set<int> cur;          //Stores ORs for subarrays ending at current index.
+            set<int> cur;          //Stores ORs for subarrays ending at current index.
             cur.insert(x);         //New subarray: only arr[i].
 
             for(auto it:prev){
@@ -13,7 +13,7 @@ public:
             }
 
             all.insert(cur.begin(),cur.end()); //Add all unique ORs for this step to global set.
-            prev=cur;              //Set up for next iteration.
+            prev=move(cur);              //Set up for next iteration.
         }
         return all.size();
     }
