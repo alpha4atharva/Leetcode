3b41a3ce3d786fcccd4583737a56e7b41a3a1e2c
Atharva Rana <ranaatharva@gmail.com>
2025-09-02 21:47:09 +0530

    Time: 19 ms (14.15%) | Memory: 19.7 MB (10.30%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>> memo;
+    
+    int dfs(vector<int>& piles, int left, int right) {
+        if (left > right) return 0;
+        if (memo[left][right] != INT_MIN) return memo[left][right];
+        
+        int leftPick = piles[left] - dfs(piles, left + 1, right);
+        int rightPick = piles[right] - dfs(piles, left, right - 1);
+        
+        return memo[left][right] = max(leftPick, rightPick);
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        memo.assign(n, vector<int>(n, INT_MIN));
+        return dfs(piles, 0, n - 1) > 0;
     }
 };
