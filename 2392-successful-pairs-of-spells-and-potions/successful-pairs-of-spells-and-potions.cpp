5109d1cfc74f8c32441adf8f072d065f6bc5f38d
Atharva Rana <ranaatharva@gmail.com>
2025-10-08 14:58:14 +0530

    Time: 47 ms (39.82%) | Memory: 128.4 MB (25.75%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bs(vector<int>&arr,long long target,int low,int high) {
+        int result = -1;
+        while(low <= high) {
+            int mid = low + (high -low) / 2;
+            if(arr[mid]>=target) {
+                result=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return result;
+    }
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        vector<int>ans;
+        int n=potions.size();
+        sort(potions.begin(),potions.end());
+        for(int i=0;i<spells.size();i++)
+        {
+            long long a=(success+spells[i]-1)/spells[i];
+            int j=bs(potions,a,0,n-1);
+            if(j==-1)   ans.push_back(0);
+            else ans.push_back(n-j);
+        }
+        return ans;
+    }
+};
