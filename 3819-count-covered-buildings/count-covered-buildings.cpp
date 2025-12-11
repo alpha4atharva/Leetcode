279d9df1d394f320daa767c5019c8a4cba350ee9
Atharva Rana <ranaatharva@gmail.com>
2025-12-11 15:53:58 +0530

    Time: 584 ms (44.75%) | Memory: 424.9 MB (30.94%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& build) {
+        unordered_map<int,vector<pair<int,int>>> x,y;
+        for(int i=0;i<build.size();i++){
+            int u=build[i][0];
+            int v=build[i][1];
+            x[u].push_back({v,i});
+            y[v].push_back({u,i});
+        }
+        
+        vector<bool> a1(build.size(),false),a2(build.size(),false);
+        for(auto &mp:x){
+            auto it=mp.second;
+            sort(it.begin(),it.end());
+            for(int i=1;i<it.size()-1;i++){
+                a1[it[i].second]=true;
+            }
+        }
+
+        for(auto &mp:y){
+            auto it=mp.second;
+            sort(it.begin(),it.end());
+            for(int i=1;i<it.size()-1;i++){
+                a2[it[i].second]=true;
+            }
+        }
+
+        int ans=0;
+        for(int i=0;i<build.size();i++){
+            if(a1[i] && a2[i]) ans++;
+        }
+        return ans;
+    }
+};
