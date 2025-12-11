ec3b1978e202f46b3cc8c84c8e81b86fab1841a0
Atharva Rana <ranaatharva@gmail.com>
2025-12-11 16:51:35 +0530

    Time: 27 ms (88.40%) | Memory: 283.7 MB (91.71%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.cpp
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -1,32 +1,58 @@
+// class Solution {
+// public:
+//     int countCoveredBuildings(int n, vector<vector<int>>& build) {
+//         unordered_map<int,vector<pair<int,int>>> x,y;
+//         for(int i=0;i<build.size();i++){
+//             int u=build[i][0];
+//             int v=build[i][1];
+//             x[u].push_back({v,i});
+//             y[v].push_back({u,i});
+//         }
+        
+//         vector<bool> a1(build.size(),false),a2(build.size(),false);
+//         for(auto &[mp,it]:x){
+//             sort(it.begin(),it.end());
+//             for(int i=1;i<it.size()-1;i++){
+//                 a1[it[i].second]=true;
+//             }
+//         }
+
+//         for(auto &[mp,it]:y){
+//             sort(it.begin(),it.end());
+//             for(int i=1;i<it.size()-1;i++){
+//                 a2[it[i].second]=true;
+//             }
+//         }
+
+//         int ans=0;
+//         for(int i=0;i<build.size();i++){
+//             if(a1[i] && a2[i]) ans++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& build) {
-        unordered_map<int,vector<pair<int,int>>> x,y;
-        for(int i=0;i<build.size();i++){
-            int u=build[i][0];
-            int v=build[i][1];
-            x[u].push_back({v,i});
-            y[v].push_back({u,i});
-        }
-        
-        vector<bool> a1(build.size(),false),a2(build.size(),false);
-        for(auto &[mp,it]:x){
-            sort(it.begin(),it.end());
-            for(int i=1;i<it.size()-1;i++){
-                a1[it[i].second]=true;
-            }
-        }
+        int m=build.size();
+        vector<int> top(n+1,INT_MAX),bottom(n+1,INT_MIN);
+        vector<int> left(n+1,INT_MAX),right(n+1,INT_MIN);
 
-        for(auto &[mp,it]:y){
-            sort(it.begin(),it.end());
-            for(int i=1;i<it.size()-1;i++){
-                a2[it[i].second]=true;
-            }
+        for(auto &it:build){
+            int x=it[0],y=it[1];
+            top[x]=min(top[x],y);
+            bottom[x]=max(bottom[x],y);
+            left[y]=min(left[y],x);
+            right[y]=max(right[y],x);
         }
 
         int ans=0;
-        for(int i=0;i<build.size();i++){
-            if(a1[i] && a2[i]) ans++;
+        for(auto &it:build){
+            int x=it[0],y=it[1];
+            if(top[x]<y && y<bottom[x] && left[y]<x && x<right[y]){
+                ans++;
+            }
         }
         return ans;
     }
