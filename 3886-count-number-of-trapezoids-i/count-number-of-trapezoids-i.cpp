3616f8545117969a0949db28a20ed252092585a4
Atharva Rana <ranaatharva@gmail.com>
2025-12-13 16:30:40 +0530

    Time: 70 ms (39.94%) | Memory: 200.1 MB (33.01%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    static const int MOD=1e9+7;
+
+    long long func(long long n){
+        return n*(n-1)/2;
+    }
+
+    int countTrapezoids(vector<vector<int>>& points) {
+        unordered_map<int,int> mp;
+        for(auto &it:points){
+            mp[it[1]]++;
+        }
+
+        long long sum=0,sumSq=0;
+        for(auto &it : mp){
+            int cnt=it.second;
+            if(cnt>1){
+                long long val=func(cnt)%MOD;   
+                sum=(sum+val)%MOD;
+                sumSq=(sumSq+val*val)%MOD;
+            }
+        }
+
+        long long ans=(sum*sum)%MOD;
+        ans=(ans-sumSq+MOD)%MOD;       
+        long long inv2=(MOD+1)/2;
+        ans=(ans*inv2)%MOD;
+
+        return ans;
+    }
+};
