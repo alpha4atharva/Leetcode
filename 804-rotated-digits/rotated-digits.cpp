f10c23c8160942e458d82ba77e5a6e0a6c99726f
Atharva Rana <ranaatharva@gmail.com>
2026-05-02 12:30:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (83.68%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public: 
     string s;
+    int memo[4][2][2];
     int recur(int pos,bool isLess,bool isRot){
         if(pos==s.size()){
             return isRot;
         }
+
+        if(memo[pos][isLess][isRot]!=-1) return memo[pos][isLess][isRot];
+
         int limit=isLess?9:s[pos]-'0';
         int count=0;
         for(int i=0;i<=limit;i++){
@@ -13,11 +17,12 @@ public:
             bool nxtRot=isRot||(i==2 || i==5 || i==6 || i==9);
             count+=recur(pos+1,nxtLess,nxtRot);
         }
-        return count;
+        return memo[pos][isLess][isRot]=count;
     }
 
     int rotatedDigits(int n) {
         s=to_string(n);
+        memset(memo,-1,sizeof(memo));
         return recur(0,false,false);
     }
 };
